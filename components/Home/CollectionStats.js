6c068fc953c1b5c04c39703e3a436c74ecb9b63d
Rafeh Qazi <>
2022-05-18 01:22:28 +0000

    quick change

--- components/Home/CollectionStats.js
+++ components/Home/CollectionStats.js
@@ -1,8 +1,14 @@
+import Image from 'next/image'
+
 const style = {
-wrapper:`grid grid-cols-2 rounded-xl border md:grid-cols-4 md:divide-x`
+  wrapper: `grid grid-cols-2 rounded-xl border md:grid-cols-4 md:divide-x`,
+  statContainer: `flex flex-col items-center justify-center px-8 py-4 md:border-0`,
+  statWithEthContainer: `flex items-center space-x-1`,
+  statItem: `text-2xl font-bold`,
+  statTitle: `text-sm text-gray-400`,
 }
 
-const CollectionStats=({ stats }) =>{
+const CollectionStats = ({ stats }) => {
   const formatStat = stat => {
     if (!stat) return
 
@@ -23,30 +29,28 @@ const CollectionStats=({ stats }) =>{
   }
 
   return (
-    <div className='grid grid-cols-2 rounded-xl border md:grid-cols-4 md:divide-x'>
-      <div className='flex flex-col items-center justify-center border-r border-b px-8 py-4 md:border-0'>
-        <p className='text-2xl font-bold'>{formatStat(stats?.total_supply)}</p>
-        <span className='text-sm text-gray-400'>items</span>
+    <div className={style.wrapper}>
+      <div className={style.statContainer}>
+        <p className={style.statItem}>{formatStat(stats?.total_supply)}</p>
+        <span className={style.statTitle}>items</span>
       </div>
-      <div className='flex flex-col items-center justify-center border-b px-8 py-4 md:border-0'>
-        <p className='text-2xl font-bold'>{formatStat(stats?.num_owners)}</p>
-        <span className='text-sm text-gray-400'>owners</span>
+      <div className={style.statContainer}>
+        <p className={style.statItem}>{formatStat(stats?.num_owners)}</p>
+        <span className={style.statTitle}>owners</span>
       </div>
-      <div className='flex flex-col items-center justify-center border-r px-8 py-4 md:border-0'>
-        <div className='flex items-center space-x-1'>
-          <img className='h-5 w-5' src='/eth-logo.svg' alt='Eth Logo' />
-          <p className='text-2xl font-bold'>{formatStat(stats?.floor_price)}</p>
+      <div className={style.statContainer}>
+        <div className={style.statWithEthContainer}>
+          <Image height={20} width={20} src='/eth-logo.svg' alt='Eth Logo' />
+          <p className={style.statItem}>{formatStat(stats?.floor_price)}</p>
         </div>
-        <span className='text-sm text-gray-400'>floor price</span>
+        <span className={style.statTitle}>floor price</span>
       </div>
-      <div className='flex flex-col items-center justify-center  px-8 py-4 '>
-        <div className='flex items-center space-x-1'>
-          <img className='h-5 w-5' src='/eth-logo.svg' alt='Eth Logo' />
-          <p className='text-2xl font-bold'>
-            {formatStat(stats?.total_volume)}
-          </p>
+      <div className={`${style.statContainer}`}>
+        <div className={style.statWithEthContainer}>
+          <Image height={20} width={20} src='/eth-logo.svg' alt='Eth Logo' />
+          <p className={style.statItem}>{formatStat(stats?.total_volume)}</p>
         </div>
-        <span className='text-sm text-gray-400'>volume traded</span>
+        <span className={style.statTitle}>volume traded</span>
       </div>
     </div>
   )
